0046cbb3fa5396c8409fe760a0939ab4c36f27c7
reinisa <reinis411@gmail.com>
2024-11-11 15:52:16 +0200

    Delete drizzle.config.js

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import ("drizzle-kit").Config } */ 
-export default {
-    schema: "./configs/schema.js", 
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: 'postgresql://carMarketplace_owner:xDm6t1nrlAOT@ep-crimson-meadow-a8pf9mbe.eastus2.azure.neon.tech/carMarketplace?sslmode=require',
-    }
-};

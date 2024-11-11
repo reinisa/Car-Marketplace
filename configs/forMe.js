6f50a5b9ce655af668c788283059f5a289cf4061
reinisa <reinis411@gmail.com>
2024-11-11 15:46:56 +0200

    Delete configs/forMe.js

--- configs/forMe.js
+++
@@ -1,5 +0,0 @@
-// schema.js 
-// createdBy:varchar('createdBy').notNull().default('ldtestreinis@gmail.com'),
-//     userName:varchar('userName').notNull().default('Reinis'),
-//     userImageUrl:varchar('userImageUrl').default('https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/3_avatar-512.png'),
-    
